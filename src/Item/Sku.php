9e4dad12f7cdc79b64a24c84c6211bbe257bf160
Camille Hougron <camille.hougron@gmail.com>
2018-02-11 20:51:24 +0100

    Add model for Skus if product with variations

--- src/Item/Sku.php
+++ src/Item/Sku.php
@@ -6,11 +6,18 @@ class Sku {
     public $id;
     public $price;
     public $qty;
+    public $model = null;
 
     public function __construct($object)
     {
         $this->id = $object->skuPropIds;
         $this->price = $object->skuVal->actSkuCalPrice;
         $this->qty = $object->skuVal->availQuantity;
+
+        if($object->skuAttr){
+            //skuAttr is in the form "14:10#3124B" with model being 3124B
+            $exploded = explode("#", $object->skuAttr);
+            $this->model = count($exploded) == 2 ? $exploded[1] : null;
+        }
     }
 }
