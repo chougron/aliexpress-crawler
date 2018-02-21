5e52b38d9e364c7ae18945be3634a2e787bbc9af
Camille Hougron <camille.hougron@gmail.com>
2018-02-21 15:06:24 +0100

    Add code sniffer

--- src/Crawler.php
+++ src/Crawler.php
@@ -1,10 +1,35 @@
 <?php
-
+/**
+ * Crawler File Doc Comment
+ *
+ * @category Class
+ * @package  Chougron\AliexpressCrawler
+ * @author   Camille Hougron <camille.hougron@gmail.com>
+ * @license  https://github.com/chougron/aliexpress-crawler/blob/master/LICENSE MIT
+ * @link     https://github.com/chougron/aliexpress-crawler
+ */
 namespace Chougron\AliexpressCrawler;
+/**
+ * Class Crawler
+ *
+ * @category Class
+ * @package  Chougron\AliexpressCrawler
+ * @author   Camille Hougron <camille.hougron@gmail.com>
+ * @license  https://github.com/chougron/aliexpress-crawler/blob/master/LICENSE MIT
+ * @link     https://github.com/chougron/aliexpress-crawler
+ */
+class Crawler
+{
 
-class Crawler {
-
-    public function crawlItem($itemUrl){
+    /**
+     * Crawl an item from a given URL
+     *
+     * @param string $itemUrl The url of the item to crawl
+     *
+     * @return Item\Crawler
+     */
+    public function crawlItem($itemUrl)
+    {
         $itemCrawler = new \Chougron\AliexpressCrawler\Item\Crawler($itemUrl);
         $itemCrawler->crawl();
         return $itemCrawler;
--- src/Item/Sku.php
+++ src/Item/Sku.php
@@ -1,20 +1,42 @@
 <?php
-
+/**
+ * Sku File Doc Comment
+ *
+ * @category Class
+ * @package  Chougron\AliexpressCrawler\Item
+ * @author   Camille Hougron <camille.hougron@gmail.com>
+ * @license  https://github.com/chougron/aliexpress-crawler/blob/master/LICENSE MIT
+ * @link     https://github.com/chougron/aliexpress-crawler
+ */
 namespace Chougron\AliexpressCrawler\Item;
-
-class Sku {
+/**
+ * Class Sku
+ *
+ * @category Class
+ * @package  Chougron\AliexpressCrawler\Item
+ * @author   Camille Hougron <camille.hougron@gmail.com>
+ * @license  https://github.com/chougron/aliexpress-crawler/blob/master/LICENSE MIT
+ * @link     https://github.com/chougron/aliexpress-crawler
+ */
+class Sku
+{
     public $id;
     public $price;
     public $qty;
     public $model = null;
 
+    /**
+     * Sku constructor.
+     *
+     * @param \stdClass $object The object with information
+     */
     public function __construct($object)
     {
         $this->id = $object->skuPropIds;
         $this->price = $object->skuVal->actSkuCalPrice;
         $this->qty = $object->skuVal->availQuantity;
 
-        if($object->skuAttr){
+        if ($object->skuAttr) {
             //skuAttr is in the form "14:10#3124B" with model being 3124B
             $exploded = explode("#", $object->skuAttr);
             $this->model = count($exploded) == 2 ? $exploded[1] : null;
